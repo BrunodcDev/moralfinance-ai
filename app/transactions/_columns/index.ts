91d9f170b73dcac1fc1570604e8fcd6d67aa97f3
BrunodcDev <brunodc.dev@gmail.com>
2024-11-07 02:52:05 -0300

    chore: commit para finalizar o dia

---
+++ app/transactions/_columns/index.ts
@@ -0,0 +1,36 @@
+"use client"
+
+import { transaction } from "@prisma/client"
+import { ColumnDef } from "@tanstack/react-table"
+
+
+export const transactioncolumns: ColumnDef<transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+  },
+  {
+    accessorKey: "catgory",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+  },
+]
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,24 @@
-const TransactionsPage = () => {
-    return <h1>transactions page</h1>;
-}
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { transactioncolumns } from "./_columns";
+import { DataTable } from "../_components/ui/data-table";
+
+const TransactionsPage = async () => {
+    const transactions = await db.transaction.findMany({})
+    return (
+        <div>
+            {/* TÍTULO E BOTÃO */}
+            <div className="flex w-full items-center justify-between p-6">
+                <h1 className="text-2x1 font-bold">Transações</h1>
+                <Button className="rounded-full">
+                    Adicionar transação
+                    <ArrowDownUpIcon />
+                </Button>
+            </div>
+            <DataTable columns={transactioncolumns} data={transactions}/>
+        </div>
+    );
+};
 
 export default TransactionsPage;
