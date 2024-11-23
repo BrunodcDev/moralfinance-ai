dc948fbb2b3eb98b708093d86391fe1483ef593b
BrunodcDev <brunodc.dev@gmail.com>
2024-11-23 01:17:36 -0300

    test: mudança de foto da pagina login e responsividade

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,31 +12,37 @@ const LoginPage = async () => {
         redirect("/")
     }
     return ( 
-        <div className="grid h-full grid-cols-2">
+        <div className="grid h-full grid-cols-1 md:grid-cols-2 gap-8 mt-5">
             {/* ESQUERDA */}
-            <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+            <div className="mx-auto flex h-full w-full max-w-[550px] flex-col justify-center p-8">
                 <Image
                     src="/Logo.png"
                     width={173}
                     height={39}
                     alt="MoralFinance AI"
+                    className="mb-4"
                 />
-                <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-                <p className="mb-8 text-muted-foreground">
+                <h1 className="mb-3 text-3xl md:text-4xl font-bold">Bem-vindo</h1>
+                <p className="mb-8 text-sm md:text-base text-muted-foreground">
                     A MoralFinance AI é uma plataforma de gestão financeira que utiliza IA para
                     monitorar suas movimentações, e oferecer insights personalizados,
                     facilitando o controle do seu orçamento.
                 </p>
                 <SignInButton>
-                    <Button variant="outline">
+                    <Button className="w-full md:w-auto" variant="outline">
                         <LogInIcon className="mr-2" />
                         Fazer Login ou Criar Conta
                     </Button>
                 </SignInButton>
             </div>
             {/* DIREITA */}
-            <div className="relative h-full w-full">
-                <Image src="/login.png" alt="Faça login" fill className="object-cover"/>
+            <div className="relative h-[300px] md:h-full w-full">
+                <Image 
+                    src="/login.png" 
+                    alt="Faça login" 
+                    fill 
+                    className="object-cover rounded-lg md:rounded-none"
+                />
             </div>
         </div>
     );
